71d91f7f0debec84992d0ac0e00c36f00253eadc
Productdb <info@productdb.io>
2024-10-04 14:54:28 +0200

    update

--- src/components/blocks/services/Services8.tsx
+++ src/components/blocks/services/Services8.tsx
@@ -7,7 +7,7 @@ import { serviceList2 } from "data/service";
 export default function Services8() {
   return (
     <div className="row gx-lg-8 gy-8 mt-5 mt-md-12 mt-lg-0 mb-15 align-items-center">
-      <div className="col-lg-3 order-lg-2">
+      <div className="col-lg-6 order-lg-2">
         <Tiles4 />
       </div>
 
@@ -19,7 +19,7 @@ export default function Services8() {
         </p>
 
         <div className="row gx-xl-10 gy-6">
-          {serviceList2.map(({ title, id, description, icon }) => (
+          {serviceList2.map(({ title, id, description,icon }) => (
             <div className="col-md-6 col-lg-12 col-xl-6" key={id}>
               <ServiceCard3
                 title={title}
